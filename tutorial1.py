f21b40e822cf6f761045a34cb0c321738a2d449a
Mir Faisal <121710726+Mirfaisal72@users.noreply.github.com>
2023-09-02 09:45:17 +0530

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -1,6 +1,6 @@
 # loading an image, resizing and rotating an image
 import cv2
-img = cv2.imread("C:/Users/MIR FAISAL/Documents/Python/Machine Learning/Open - CV/EYE.jpg",-1)
+img = cv2.imread("C:/Users/NAME/Documents/Python/Machine Learning/Open - CV/EYE.jpg",-1)
 img = cv2.resize(img,(400,400))
 img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
 cv2.imshow('Image',img)
